0a36027bb73f05c673e4d63e6bfff319aece5105
Charles Martin Reed <charlesmartinreed@icloud.com>
2019-01-23 17:36:41 -0600

    Initial Commit

---
+++ JSON Decoding in Swift 4+/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSON Decoding in Swift 4+
+//
+//  Created by Charles Martin Reed on 1/23/19.
+//  Copyright © 2019 Charles Martin Reed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
