bac28eeac2fbcbcf6dbb70c6369fea3d0d401748
Charles Martin Reed <charlesmartinreed@icloud.com>
2019-01-23 18:22:07 -0600

    Implement optional properties in data struct to account for situations where JSON data for property is not available.

--- JSON Decoding in Swift 4+/ViewController.swift
+++ JSON Decoding in Swift 4+/ViewController.swift
@@ -11,23 +11,32 @@ import UIKit
 class ViewController: UIViewController {
 
     struct Course: Decodable {
-        let id: Int
-        let name: String
-        let link: String
-        let imageUrl: String
+        let id: Int?
+        let name: String?
+        let link: String?
+        let imageUrl: String?
+
+        //because JSONDecoder is able to automatically match json properties to our properties above, the init is actually extraneous in modern Swift
         
-        init(json: [String: Any]) {
-            id = json["id"] as? Int ?? -1
-            name = json["name"] as? String ?? ""
-            link = json["link"] as? String ?? ""
-            imageUrl = json["imageUrl"] as? String ?? ""
-        }
+//        init(json: [String: Any]) {
+//            id = json["id"] as? Int ?? -1
+//            name = json["name"] as? String ?? ""
+//            link = json["link"] as? String ?? ""
+//            imageUrl = json["imageUrl"] as? String ?? ""
+//        }
+    }
+    
+    struct WebsiteDescription: Decodable {
+        let name: String
+        let description: String
+        let courses: [Course]
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let jsonURLString = "https://api.letsbuildthatapp.com/jsondecodable/course"
+        //for this URL, some of the json categories will be missing. We can handle this by making our data model properties optional.
+        let jsonURLString = "https://api.letsbuildthatapp.com/jsondecodable/courses_missing_fields"
         guard let url = URL(string: jsonURLString) else { return }
         fetchJSONData(for: url)
     
@@ -43,9 +52,12 @@ class ViewController: UIViewController {
             
             //Swift 4+ means - make sure your data model conforms to Decodable
             do {
-                let course = try JSONDecoder().decode(Course.self, from: data)
-                print(course.name)
+//                let websiteDescription = try JSONDecoder().decode(WebsiteDescription.self, from: data)
                 
+                let courses = try JSONDecoder().decode([Course].self, from: data)
+                //obviously we'd want to handle the optionality in a more meaningful way here in a production app
+                print(courses)
+//
                 // serializing data in the old, pre Swift-4 ways - Swift 2/3/Obj-C
                 // guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String : Any] else { fatalError("Could not retrieve json data")}
 //                let course = Course(json: json)
